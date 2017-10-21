ec412dc66f50c0fa94429ddced6b9030321ff0cf
HirdayGupta <hirday.gupta@gmail.com>
2017-10-21 02:12:53 -0400

    Add FB Login Code

--- timeboARd/LoginViewController.swift
+++ timeboARd/LoginViewController.swift
@@ -7,7 +7,29 @@
 //
 
 import Foundation
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class LoginViewController: UIViewController {
+    private let dataURL = "https://infra-mix-183600.firebaseio.com/"
     
+    @IBAction func facebookLogin(sender: AnyObject) {
+        
+        //Create a reference to a firebase location
+        let myRootRef = Firebase(url: dataURL)
+        
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler:{(facebookResult, facebookError) -> Void in
+            if facebookError != nil {
+                print ("Facebook Login Failed") // indicative placeholder, to be handled
+            } else if (facebookResult?.isCancelled)! {
+                print ("Facebook Login Cancelled") // indicative placeholder, to be handled
+            } else {
+                // TODO: Firebase Auth
+                // TODO: Proceed to next VC on success
+                print ("Facebook Login Success")
+            }
+        });
+    }
 }
