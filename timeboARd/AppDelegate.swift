d4f847c9e1dbcacd48d7a2d1936c41bc9479f80f
HirdayGupta <hirday.gupta@gmail.com>
2017-10-21 08:20:37 -0400

    Initialise Firebase in AppDelegate

--- timeboARd/AppDelegate.swift
+++ timeboARd/AppDelegate.swift
@@ -9,6 +9,7 @@
 import UIKit
 import FBSDKCoreKit
 import FBSDKLoginKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         return FBSDKApplicationDelegate.sharedInstance().application(application,didFinishLaunchingWithOptions: launchOptions)
     }
 
