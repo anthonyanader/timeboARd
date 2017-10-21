bcd228778ea704a7b0d5a7dc996f7e31cca5f993
HirdayGupta <hirday.gupta@gmail.com>
2017-10-21 08:29:23 -0400

    Update loginScript with updated FB and firebase API

--- timeboARd/LoginViewController.swift
+++ timeboARd/LoginViewController.swift
@@ -12,24 +12,42 @@ import FBSDKLoginKit
 import Firebase
 
 class LoginViewController: UIViewController {
-    private let dataURL = "https://infra-mix-183600.firebaseio.com/"
     
     @IBAction func facebookLogin(sender: AnyObject) {
-        
-        //Create a reference to a firebase location
-        let myRootRef = Firebase(url: dataURL)
-        
-        let facebookLogin = FBSDKLoginManager()
-        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler:{(facebookResult, facebookError) -> Void in
-            if facebookError != nil {
-                print ("Facebook Login Failed") // indicative placeholder, to be handled
-            } else if (facebookResult?.isCancelled)! {
-                print ("Facebook Login Cancelled") // indicative placeholder, to be handled
-            } else {
-                // TODO: Firebase Auth
-                // TODO: Proceed to next VC on success
-                print ("Facebook Login Success")
+        let fbLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["public_profile", "email"], from: self) { (result, error) in
+            if let error = error {
+                print("Failed to login: \(error.localizedDescription)")
+                return
             }
-        });
+            
+            guard let accessToken = FBSDKAccessToken.current() else {
+                print("Failed to get access token")
+                return
+            }
+            
+            let credential = FacebookAuthProvider.credential(withAccessToken: accessToken.tokenString)
+            
+            // Perform login by calling Firebase APIs
+            Auth.auth().signIn(with: credential, completion: { (user, error) in
+                if let error = error {
+                    print("Login error: \(error.localizedDescription)")
+                    let alertController = UIAlertController(title: "Login Error", message: error.localizedDescription, preferredStyle: .alert)
+                    let okayAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(okayAction)
+                    self.present(alertController, animated: true, completion: nil)
+                    
+                    return
+                }
+                
+                // Present the main view
+                if let viewController = self.storyboard?.instantiateViewController(withIdentifier: "MainView") {
+                    UIApplication.shared.keyWindow?.rootViewController = viewController
+                    self.dismiss(animated: true, completion: nil)
+                }
+                
+            })
+            
+        }
     }
 }
