01707e6a52127669613e16df24eac0691254559a
anthonyanader <anthonyanader@gmail.com>
2017-10-21 14:23:11 -0400

    testing new animator UI. Added a controller file for each view

---
+++ timeboARd/SaveBoardViewController.swift
@@ -0,0 +1,10 @@
+//
+//  SaveBoardViewController.swift
+//  timeboARd
+//
+//  Created by Anthony A. Nader on 2017-10-21.
+//  Copyright © 2017 Anthony A. Nader. All rights reserved.
+//
+
+import Foundation
+import UIKit
---
+++ timeboARd/TimeMachineViewController.swift
@@ -0,0 +1,14 @@
+//
+//  TimeMachineViewController.swift
+//  timeboARd
+//
+//  Created by Anthony A. Nader on 2017-10-21.
+//  Copyright © 2017 Anthony A. Nader. All rights reserved.
+//
+
+import Foundation
+
+class TimeMachineViewController: UIViewController {
+    
+    
+}
