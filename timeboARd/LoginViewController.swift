98ed3f6f7d0ea5f9c6c1efd0d357dfb103e2419f
HirdayGupta <hirday.gupta@gmail.com>
2017-10-21 01:32:18 -0400

    Link LoginViewController to a new bare-bones class

---
+++ timeboARd/LoginViewController.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.swift
+//  timeboARd
+//
+//  Created by Hirday Gupta on 21/10/17.
+//  Copyright © 2017 Anthony A. Nader. All rights reserved.
+//
+
+import Foundation
+
+class LoginViewController: UIViewController {
+    
+}
